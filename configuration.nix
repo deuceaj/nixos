1575897c6ea4921e1703237e3f1228f1597b358e
deuce <aj.doucett@gmail.com>
2023-04-05 18:57:04 -0400

    g

--- configuration.nix
+++ configuration.nix
@@ -4,156 +4,154 @@
 
 { config, pkgs, ... }:
 
-
-let
-   user="deuce";
-in
-   
-
-
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-#      <home-manager/nixos>
     ];
 
-   # Set the system state version
-  system.stateVersion = "22.05";
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  # Configure GRUB with dual boot
-  boot.loader.grub = {
-    enable = true;
-    version = 2;
-    useOSProber = true;
-    devices = [ "/dev/sda" ];
-    extraEntries = ''
-      menuentry "Windows 10" {
-        insmod ntfs
-        search --no-floppy --fs-uuid --set=root 201C496E1C493FD0
-        chainloader +1
-      }
-    '';
-  };
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Set the hostname
-  networking.hostName = "Alpha";
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Set the timezone
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Configure the user
-  users.users.${user} = {
-    isNormalUser = true;
-    hashedPassword = "${pkgs.lib.mkpasswd "hello"}";
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
   };
 
-# Allow unfree packages
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the XFCE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.xfce.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "";
+  };
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.deuce = {
+    isNormalUser = true;
+    description = "deuce";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+    #  thunderbird
+    ];
+  };
+
+  programs.corectrl.enable = true;
+  programs.corectrl.gpuOverclock.enable = true;
+  programs.corectrl.gpuOverclock.ppfeaturemask = "0xffffffff";
+  services.corectrl.enable = true;
+
+  services.polkit.enable = true;
+  security.polkit.enable = true;
+
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
+    
     alacritty
     bspwm
-    btop
-    corectrl
-    cinnamon.nemo    
-    discord
-    dunst
+    cinnamon.nemo
     firefox-devedition-bin
     neovim
     nitrogen
     polkit-gnome
-    sddm
-    sxhkd
+    polybar
     rofi
+    steam
+    sxhkd
     wget
-    vscode  
-
-  #     thunderbird
-     ];
-  # };
-
-
-  # Set up SDDM as the default display manager.
-  services = {
-    displayManager ={
-     sddm = {enable = true;
-     };
+    vscode     
     
-    xserver = {
-    
-    displayManager = {
-      sddm = {
-        enable = true;
-      };
-    };
-    windowManager = {
-      bspwm = {
-        enable = true;
-        };
-    };
-      desktopManager = {
-        sxhkd = {
-          enable = true;
-        };
-        rofi = {
-          enable = true;
-        };
-        nitrogen = {
-          enable = true;
-        };
-    };
- 
-    corectrl = {
-      enable = true;
-    };
+  ];
 
-    polkit = {
-        enable = true;
-      };
-    };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+ # programs.mtr.enable = true;
+ # programs.gnupg.agent = {
+ #   enable = true;
+ #   enableSSHSupport = true;
+ #   programs.corectrl.enable = true;
+ # };
 
-  security = {
-   polkit = {
-    enable = true; 
-   };
-  };
-  
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.xserver.windowManager.bspwm.enable = true;
 
 
 
 
- ################################################
-  # Enable sound.
-  ################################################
-   sound = {
-      enable = true;
-      mediaKeys.enable = true;
-    };
-    services = {
-      pipewire = {
-        enable = true;
-        alsa = {
-          enable = true;
-          support32Bit = true;
-        };
-        pulse.enable = true;
-      };
-    };
-    hardware = {
-      bluetooth = {
-        enable = true;
-        hsphfpd.enable = false;         # HSP & HFP daemon
-        settings = {
-          General = {
-            Enable = "Source,Sink,Media,Socket";
-          };
-        };
-      };
-    };
 
 
-};
-};
+ # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+
 }
-
