ee20132dcfebfdc93f2fbae654a3f6257dcf4c9a
deuce <aj.doucett@gmail.com>
2023-04-18 20:01:35 -0400

    g

--- format-vm.sh
+++ format-vm.sh
@@ -2,7 +2,7 @@
 parted /dev/vda -- mklabel gpt
 
 # Create Partitions
-parted /dev/vda -- mkpart primary 512MiB -8GiB 
+parted /dev/vda -- mkpart primary 512MiB -8GiB
 parted /dev/vda -- mkpart primary linux-swap -8GiB 100%
 parted /dev/vda -- mkpart ESP fat32 1Mib 512MiB
 parted /dev/vda -- set 3 esp on
@@ -14,8 +14,8 @@ mkfs.fat -F 32 -n boot /dev/vda3
 
 # Mount Partitions
 mount /dev/disk/by-label/nixos /mnt
-mkdir -p /mnt/boot
-mount /dev/disk/by-label/boot /mnt/boot
+mkdir -p /mnt/boot/efi
+mount /dev/disk/by-label/boot /mnt/boot/efi
 swapon /dev/vda2
 
 # Generate Nixos Config file
